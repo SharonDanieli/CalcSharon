e99c03f4ae213fe46d2c0f28311e27a8c1483bda
SharonDanieli <fam2.danieli@gmail.com>
2018-05-04 19:59:53 +0300

    5st commit: java

--- app/src/main/java/com/example/user/calcsharon/MainActivity.java
+++ app/src/main/java/com/example/user/calcsharon/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.user.calcsharon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,25 +32,66 @@ public class MainActivity extends AppCompatActivity {
         cre = (Button) findViewById(R.id.cre);
     }
 
-    int ope=0;
-    public static void hibur (int num1, int num2){
-
-    }
-    public static void hisur (int num1, int num2){
-
-    }
-    public static void kefel (int num1, int num2){
-
-    }
-    public static void hiluk (int num1, int num2){
+    public void main(String[]args){
+        savetonum1(num1);//הזנת מס' ראשון ושמירתו בnum1
+        //לחיצה על פעולה
+        savetonum2(num2);//הזנת מס' שני ושמירתו בnum2
 
     }
 
+    int num1=0, num2=0;
+    public void savetonum1(int num1){
+        String st = et.getText().toString();
+        num1 = Integer.parseInt(st);
+    }
 
-    public static void arithmetic(int num1, int num2){
-        switch (int ope){
-            case 1: 
+    public void savetonum2(int num2){
+        String st = et.getText().toString();
+        num2 = Integer.parseInt(st);
+    }
+
+    int operation=0;
+    public void hibur(View view) {
+        et.setText("");
+        operation=1;
+    }
+
+    public void hisur(View view) {
+        et.setText("");
+        operation=2;
+    }
+
+    public void kefel(View view) {
+        et.setText("");
+        operation=3;
+    }
+
+    public void hiluk(View view) {
+        et.setText("");
+        operation=4;
+    }
+
+    public void shave(View view) {
+        et.setText("");
+        switch (operation) {
+            case 1:
+                num1+=num2;
+                et.setText(num1+num2);
+                break;
+            case 2:
+                num1-=num2;
+                et.setText(num1-num2);
+                break;
+            case 3:
+                num1*=num2;
+                et.setText(num1*num2);
+                break;
+            case 4:
+                num1/=num2;
+                et.setText(num1/num2);
+                break;
+            default:
+                Toast.makeText(this, "please click operation", Toast.LENGTH_SHORT).show();
         }
     }
 }
-
