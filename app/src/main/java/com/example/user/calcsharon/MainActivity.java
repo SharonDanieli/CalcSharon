c077329c8ca50475a087dcd17dd7542e11ba6eb1
SharonDanieli <fam2.danieli@gmail.com>
2018-05-13 23:03:31 +0300

    7st commit: credits

--- app/src/main/java/com/example/user/calcsharon/MainActivity.java
+++ app/src/main/java/com/example/user/calcsharon/MainActivity.java
@@ -1,28 +1,29 @@
 package com.example.user.calcsharon;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    Scanner in = new Scanner(System.in);
 
     EditText et;
-    Button plus;
-    Button minus;
-    Button multi;
-    Button divi;
-    Button delete;
-    Button equal;
-    Button cre;
+    Button plus, minus, multi, divi, delete, equal, cre;
+    Double num1;
+    Double num2;
+    Boolean num1exist;
+    int operation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         et = (EditText) findViewById(R.id.et);
         plus = (Button) findViewById(R.id.plus);
         minus = (Button) findViewById(R.id.minus);
@@ -31,55 +32,55 @@ public class MainActivity extends AppCompatActivity {
         delete = (Button) findViewById(R.id.delete);
         equal = (Button) findViewById(R.id.equal);
         cre = (Button) findViewById(R.id.cre);
+
+        num1exist=false;
+        operation=0;
     }
 
-    String st = et.getText().toString();
-    public void savetonum1(int num1){
+
+    public void savetonum() {
+        Double num;
         String st = et.getText().toString();
-        num1 = Integer.parseInt(st);
+        //check input- if wrong toast
+        num = Double.parseDouble(st);
+        num1exist=true;// set the flag to true for 1st operand return the double
     }
 
-    int num1=0;
-    int operation=0;
+
     public void hibur(View view) {
-        et.setText("");
+        if(num1exist) {
+            String st = et.getText().toString();
+            num2 = Double.parseDouble(st);//enter the number from et to num2
+            num1+=num2;//calc operation between 1 & 2 and store result in num1
+            et.setText(Double.toString(num1));//display num1
+        } else {
+            String st = et.getText().toString();
+            num1 = Double.parseDouble(st);//enter the number from et to num1
+            num1exist=true;
+            operation=1;
+        }
+        // check if there is first num
+/*        et.setText("");
         num1+=Integer.parseInt(st);
-        operation=1;
+        operation=1;*/
     }
 
-    public void hisur(View view) {
-        et.setText("");
-        num1-=Integer.parseInt(st);
-        operation=2;
-    }
-
-    public void kefel(View view) {
-        et.setText("");
-        num1*=Integer.parseInt(st);
-        operation=3;
-    }
-
-    public void hiluk(View view) {
-        et.setText("");
-        num1/=Integer.parseInt(st);
-        operation=4;
-    }
 
 
     public void shave(View view) {
         et.setText("");
         switch (operation) {
             case 1:
-                et.setText(num1+Integer.parseInt(st));
+                et.setText((int) (num1+num2));
                 break;
             case 2:
-                et.setText(num1-Integer.parseInt(st));
+                et.setText((int) (num1- num2));
                 break;
             case 3:
-                et.setText(num1*Integer.parseInt(st));
+                et.setText((int) (num1*num2));
                 break;
             case 4:
-                et.setText(num1/Integer.parseInt(st));
+                et.setText((int) (num1/num2));
                 break;
             default:
                 Toast.makeText(this, "please click operation", Toast.LENGTH_SHORT).show();
@@ -88,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void cleartext(View view) {
         et.setText("");
-        num1=0;
+        num1=0.0;
     }
 
     public void gocredit(View view) {
