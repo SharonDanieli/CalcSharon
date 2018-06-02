e999af3fa3b2e504c293466c73c1567bd599139f
SharonDanieli <fam2.danieli@gmail.com>
2018-06-02 15:49:29 +0300

    8st commit

--- app/src/main/java/com/example/user/calcsharon/MainActivity.java
+++ app/src/main/java/com/example/user/calcsharon/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText et;
     Button plus, minus, multi, divi, delete, equal, cre;
-    Double num1;
-    Double num2;
+    Double num1 = 0.0;
+    Double num2 = 0.0;
     Boolean num1exist;
     int operation;
 
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         equal = (Button) findViewById(R.id.equal);
         cre = (Button) findViewById(R.id.cre);
 
-        num1exist=false;
-        operation=0;
+        num1exist = false;
+        operation = 0;
     }
 
 
@@ -43,57 +43,142 @@ public class MainActivity extends AppCompatActivity {
         String st = et.getText().toString();
         //check input- if wrong toast
         num = Double.parseDouble(st);
-        num1exist=true;// set the flag to true for 1st operand return the double
+        num1exist = true;// set the flag to true for 1st operand return the double
     }
 
 
     public void hibur(View view) {
-        if(num1exist) {
-            String st = et.getText().toString();
-            num2 = Double.parseDouble(st);//enter the number from et to num2
-            num1+=num2;//calc operation between 1 & 2 and store result in num1
-            et.setText(Double.toString(num1));//display num1
+        if ((!et.getText().toString().equals("")) && ((!et.getText().toString().equals(".")) && (!et.getText().toString().equals("-")) && (!et.getText().toString().equals("-.")))) {
+            if (num1exist) {
+                String st = et.getText().toString();
+                num2 = Double.parseDouble(st);//enter the number from et to num2
+                et.setText("");
+                num1 += num2;//calc operation between 1 & 2 and store result in num1
+                et.setHint("" + num1);//display num1
+            } else {
+                String st = et.getText().toString();
+                et.setText("");
+                num1 = Double.parseDouble(st);//enter the number from et to num1
+                num1exist = true;
+            }
         } else {
-            String st = et.getText().toString();
-            num1 = Double.parseDouble(st);//enter the number from et to num1
-            num1exist=true;
-            operation=1;
+            Toast.makeText(this, "input is unvailable", Toast.LENGTH_SHORT).show();
         }
-        // check if there is first num
-/*        et.setText("");
-        num1+=Integer.parseInt(st);
-        operation=1;*/
+        operation = 1;
     }
 
+    public void hisur(View view) {
+        if ((!et.getText().toString().equals("")) && ((!et.getText().toString().equals(".")) && (!et.getText().toString().equals("-")) && (!et.getText().toString().equals("-.")))) {
+            if (num1exist) {
+                String st = et.getText().toString();
+                num2 = Double.parseDouble(st);//enter the number from et to num2
+                et.setText("");
+                num1 -= num2;//calc operation between 1 & 2 and store result in num1
+                et.setHint("" + num1);//display num1
+            } else {
+                String st = et.getText().toString();
+                et.setText("");
+                num1 = Double.parseDouble(st);//enter the number from et to num1
+                num1exist = true;
+            }
+        } else {
+            Toast.makeText(this, "input is unvailable", Toast.LENGTH_SHORT).show();
+        }
+        operation = 2;
+    }
+
+    public void kefel(View view) {
+        if ((!et.getText().toString().equals("")) && ((!et.getText().toString().equals(".")) && (!et.getText().toString().equals("-")) && (!et.getText().toString().equals("-.")))) {
+            if (num1exist) {
+                String st = et.getText().toString();
+                num2 = Double.parseDouble(st);//enter the number from et to num2
+                et.setText("");
+                num1 += num2;//calc operation between 1 & 2 and store result in num1
+                et.setHint("" + num1);//display num1
+            } else {
+                String st = et.getText().toString();
+                et.setText("");
+                num1 = Double.parseDouble(st);//enter the number from et to num1
+                num1exist = true;
+            }
+        } else {
+            Toast.makeText(this, "input is unvailable", Toast.LENGTH_SHORT).show();
+        }
+        operation = 1;
+    }
+
+    public void hiluk(View view) {
+        if ((!et.getText().toString().equals("")) && ((!et.getText().toString().equals(".")) && (!et.getText().toString().equals("-")) && (!et.getText().toString().equals("-.")))) {
+            if (num1exist) {
+                String st = et.getText().toString();
+                num2 = Double.parseDouble(st);//enter the number from et to num2
+                et.setText("");
+                num1 += num2;//calc operation between 1 & 2 and store result in num1
+                et.setHint("" + num1);//display num1
+            } else {
+                String st = et.getText().toString();
+                et.setText("");
+                num1 = Double.parseDouble(st);//enter the number from et to num1
+                num1exist = true;
+            }
+        } else {
+            Toast.makeText(this, "input is unvailable", Toast.LENGTH_SHORT).show();
+        }
+        operation = 1;
+    }
+    // check if there is first num
+/*
+        num1+=Integer.parseInt(st);
+        operation=1;*/
 
 
     public void shave(View view) {
-        et.setText("");
-        switch (operation) {
-            case 1:
-                et.setText((int) (num1+num2));
-                break;
-            case 2:
-                et.setText((int) (num1- num2));
-                break;
-            case 3:
-                et.setText((int) (num1*num2));
-                break;
-            case 4:
-                et.setText((int) (num1/num2));
-                break;
-            default:
-                Toast.makeText(this, "please click operation", Toast.LENGTH_SHORT).show();
+        if ((!et.getText().toString().equals("")) && ((!et.getText().toString().equals(".")) && (!et.getText().toString().equals("-")) && (!et.getText().toString().equals("-.")))) {
+            double res = 0;
+            switch (operation) {
+                case 1:
+                    String st1 = et.getText().toString();
+                    num2 = Double.parseDouble(st1);
+                    res = num1 + num2;
+                    break;
+                case 2:
+                    st1 = et.getText().toString();
+                    num2 = Double.parseDouble(st1);
+                    res = num1 - num2;
+                    break;
+                case 3:
+                    st1 = et.getText().toString();
+                    num2 = Double.parseDouble(st1);
+                    res = num1 * num2;
+                    break;
+                case 4:
+                    st1 = et.getText().toString();
+                    num2 = Double.parseDouble(st1);
+                    if (num2 == 0)
+                        Toast.makeText(this, "math ERROR", Toast.LENGTH_SHORT).show();
+                    res = num1 / num2;
+                    break;
+                default:
+                    Toast.makeText(this, "please click operation", Toast.LENGTH_SHORT).show();
+                    et.setText("" + res);
+            }
         }
     }
 
     public void cleartext(View view) {
         et.setText("");
-        num1=0.0;
+        num1=0.0; num2=0.0; operation=0;
     }
 
-    public void gocredit(View view) {
+    public void goCredit(View view) {
         Intent t = new Intent(this, credits.class);
+        String str = Double.toString(num1);
+        t.putExtra("result", str);
         startActivity(t);
     }
 }
+
+
+
+
+
